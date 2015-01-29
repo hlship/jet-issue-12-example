f84bd2f9c8be3cd83c8fdae646824d0b5c5594a0
Howard M. Lewis Ship <hlship@gmail.com>
2015-01-29 14:18:19 -0800

    Add more to the processing stack, in an attempt to provoke the EofException

--- project.clj
+++ project.clj
@@ -1,19 +1,20 @@
 (defproject example/jet-issue-12 "0.0.1"
 
-  :main main
+            :main main
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+            :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
 
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                           [org.clojure/core.async "0.1.346.0-17112a-alpha"]
 
-                 [cc.qbits/jet "0.5.4"]
+                           [cc.qbits/jet "0.5.4"]
 
-                 [ring/ring-core "1.3.2"]
-                 [ring-middleware-format "0.4.0"]
+                           [ring/ring-core "1.3.2"]
 
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-api "1.7.6"]
-                 [ch.qos.logback/logback-classic "1.1.1"]
+                           [org.clojure/tools.logging "0.3.1"]
+                           [org.slf4j/slf4j-api "1.7.6"]
+                           [ch.qos.logback/logback-classic "1.1.1"]
 
-                 [io.aviso/toolchest "0.1.1"]
-                 [io.aviso/pretty "0.1.14"]])
+                           [io.aviso/toolchest "0.1.1"]
+                           [io.aviso/pretty "0.1.14"]
+                           [io.aviso/rook "0.1.23"]
+                           [com.cognitect/transit-clj "0.8.259"]])
--- src/server.clj
+++ src/server.clj
@@ -2,19 +2,14 @@
   (:require logging-setup
             [qbits.jet.server :as jet]
             [clojure.core.async :refer [thread]]
+            [io.aviso.rook.async :as a]
+            [io.aviso.rook.server :as s]
             [ring.util.response :as r]
-            [ring.middleware.format :as f]
             [clojure.tools.logging :as l]))
 
 (defn core-handler [request]
-  (l/info "async-handler" (:request-method request) (:uri request))
+  (l/info "core-handler" (:request-method request) (:uri request))
   (r/response (select-keys request [:request-method :uri])))
-
-
-(defn wrap-async
-  [handler]
-  (fn [request]
-    (thread (handler request))))
 
 ;; Test with:
 ;;   ab -c 20 -k -t 10 localhost:8080/hello
@@ -22,8 +17,9 @@
 (defn launch
   []
   (let [handler (-> core-handler
-                    (f/wrap-restful-format :formats [:json :edn])
-                    wrap-async)]
+                    a/ring-handler->async-handler
+                    (s/wrap-with-timeout 100)
+                    a/wrap-with-standard-middleware)]
     (jet/run-jetty {:join?        false
                     :port         8080
                     :ring-handler handler})))
